44a3311462858701227a95d7fd09a67804ca9659
inari2807 <0165sakk@gmail.com>
2020-09-25 23:03:33 +0700

    Create b2 bai tap 6.cpp

---
+++ b2 bai tap 6.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int n,S=0;
+	printf("nhap n=");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		if(a[i]>0){
+			min = a[i]
+			break;
+		}
+	}
+	for(i=0;i<n;i++){
+		if(a[i]>0 && a[i]<min)
+		
+		min = a[i];
+	}
+	return min
+}
